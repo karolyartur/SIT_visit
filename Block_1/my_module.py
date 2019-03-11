742c6f3d7e3580899fcfc88ee1cfa482a1bd67aa
Artur Karoly <artur.karoly@irob.uni-obuda.hu>
2019-03-11 12:55:18 +0100

    Create lesson 1.3 and excersise 1.3

---
+++ Block_1/my_module.py
@@ -0,0 +1,15 @@
+'''Docstrings for the whole module
+Explaining what the module can be used for'''
+
+def my_first_function():
+    '''Docstrings for my _first_function'''
+    print('my_first_function is called')
+
+def my_second_function():
+    '''Docstrings for my _second_function'''
+    print('my_second_function is called')
+
+variable1 = 10
+variable2 = [1,2,3,4]
+
+print('The code inside my_module is executed')
